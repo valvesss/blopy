b40a92990de7cd82007ea81526db2993904b0bfd
valves <vitoralvesinfo@gmail.com>
2019-09-26 01:04:10 -0300

    Fix to threads and new connections

--- server_tcp.py
+++ server_tcp.py
@@ -8,41 +8,32 @@ import threading
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s :: %(levelname)s :: %(message)s')
 
-class PeerServer():
+class PeerServer(threading.Thread):
     def __init__(self):
+        super(PeerServer, self).__init__()
         self.__host__ = get_ip()
         self.__port__ = 5000
-        self.__close_flag__ = False
+        self.__stop_flag__ = threading.Event()
         self.__nodesIn__ = []
         self.timeout = 20
-        self.start()
+        self.scale_up()
 
-    def start(self):
+    def scale_up(self):
         self.__sock__ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.__sock__.bind(('', self.__port__))
         self.__sock__.listen()
         self.__sock__.settimeout(self.timeout)
         logging.info('PeerServer is waiting for income connections.')
 
-    def run(self):
-        while not self.__close_flag__:
-            try:
-                peer_socket, peer_addr = self.__sock__.accept()
-                with peer_socket:
-                    new_inbound_peer = PeerNode(self.__host__, peer_socket, peer_addr, len(self.__nodesIn__))
-                    # new_inbound_peer.start()
-                    self.__nodesIn__.append(new_inbound_peer)
-            except socket.timeout:
-                self.close_server_connection('timeout')
-
-        self.close_server_connection('finished run.')
-
     def send_to_nodes(self):
-        for node in self.__nodesIn__:
-            node.send()
+        if self.__nodesIn__:
+            for node in self.__nodesIn__:
+                node.send()
+        else:
+            logging.info('There are no InboundNodes! Sending message to nodes failed.')
 
     def close_server_connection(self, msg):
-        if not self.__close_flag__:
+        if not self.__stop_flag__.is_set():
             self.close_connected_nodes()
             self.stop()
             self.__sock__.close()
@@ -51,11 +42,29 @@ class PeerServer():
 
     def close_connected_nodes(self):
         for nodes in self.__nodesIn__:
-            nodes.close_connection('PeerServer being shut down')
+            if not nodes.__stop_flag__.is_set():
+                nodes.close_connection('PeerServer being shut down')
+                nodes.join()
 
     def stop(self):
-        if not self.__close_flag__:
-            self.__close_flag__ = True
+        if not self.__stop_flag__.is_set():
+            self.__stop_flag__.set()
+
+    def run(self):
+        while not self.__stop_flag__.is_set():
+            try:
+                peer_socket, peer_addr = self.__sock__.accept()
+                with peer_socket:
+                    new_inbound_peer = PeerNode(self.__host__, peer_socket, peer_addr, len(self.__nodesIn__))
+                    new_inbound_peer.start()
+                    self.__nodesIn__.append(new_inbound_peer)
+                    logging.info('New node appended to InboundNodes.')
+                    # time.sleep(5)
+                    new_inbound_peer.send()
+            except socket.timeout:
+                self.close_server_connection('timeout')
+
+        self.close_server_connection('finished run.')
 
 class PeerNode(threading.Thread):
     def __init__(self, serverHost, sock, addr, index):
@@ -66,15 +75,17 @@ class PeerNode(threading.Thread):
         self.__port__ = addr[1]
         self.index = index
         self.__buffer__ = ""
-        self.__close_flag__ = False
-        self.timeout = 10
-
+        self.__stop_flag__ = threading.Event()
+        self.timeout = 20
         logging.info('Server connected to InPeerNode: #{0} {1}:{2}.'.format(self.index,self.__host__, self.__port__))
-        self.run()
+
+    def stop(self):
+        if not self.__stop_flag__.is_set():
+            self.__stop_flag__.set()
 
     def run(self):
         self.__sock__.settimeout(self.timeout)
-        while not self.__close_flag__:
+        while not self.__stop_flag__.is_set():
             packets = ""
             try:
                 packets = self.__sock__.recv(1024)
@@ -86,28 +97,37 @@ class PeerNode(threading.Thread):
             if packets != "":
                 try:
                     self.__buffer__ += packets
+                    logging.info('Server received a message from InPeerNode #{0}.'.format(self.index))
                 except:
                     logging.info('InPeerNode #{0}: Error decoding message: {1}{2}'.format(self.index,packets,type(packets)))
                     self.stop()
-                self.send()
 
         self.close_connection('finished run')
 
     def send(self):
-        data = self.__buffer__
-        data = data.encode('ascii')
-        self.__sock__.sendall(data)
-        logging.info('Server sent a message to InPeerNode: #{0}.'.format(self.index))
+        for i in range(5):
+            if not self.__stop_flag__.is_set():
+                if self.__buffer__:
+                    data = self.__buffer__
+                    data = data.encode('ascii')
+                    try:
+                        self.__sock__.sendall(data)
+                    except Exception as err:
+                        logging.info('Server failed to sent a message to InPeerNode: #{0}. \nError: {1}'.format(self.index,err))
+                    logging.info('Server sent a message to InPeerNode: #{0}.'.format(self.index))
+                    self.close_connection('message sent')
+                    return False
+                else:
+                    logging.error('Server has no buffer! Send message to InPeerNode #{0} failed. Try: {1}'.format(self.index,i+1))
+                    time.sleep(1)
+            else:
+                logging.error('Could not sent message! InPeerNode #{0} already shut down!'.format(self.index))
 
     def close_connection(self, msg):
-        if not self.__close_flag__:
+        if not self.__stop_flag__.is_set():
             self.stop()
             self.__sock__.close()
             logging.info('InPeerNode #{0} has been disconnected due to {1}.'.format(self.index,msg))
-
-    def stop(self):
-        if not self.__close_flag__:
-            self.__close_flag__ = True
 
 def get_ip():
     if os.name == 'nt':
@@ -121,9 +141,6 @@ def get_ip():
 if __name__ == '__main__':
     peer = PeerServer()
     try:
-        peer.run()
-        # peer.send_to_nodes()
+        peer.start()
     except KeyboardInterrupt:
         peer.close_server_connection('KeyboardInterrupt')
-
-main()
