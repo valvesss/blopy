0a263c7cf07f9cde1140a54c87f83c94d87747df
valves <vitoralvesinfo@gmail.com>
2019-10-05 16:53:25 -0300

    Added support to requests

--- blopy/client2.py
+++ blopy/client2.py
@@ -1,11 +1,13 @@
 import time
 
+from pprint import pprint
 from blockchain import *
 from server import Server
 
-alice = Server('localhost', 6000, 15)
+alice = Server('localhost', 6000, 20)
 alice.start()
 
-time.sleep(3)
-
-alice.send_to_nodes('FLAG_01')
+alice.bc.create_genesis_block('IM ALICE')
+alice.send_to_nodes('REQ_01')
+time.sleep(5)
+alice.send_to_nodes('REQ_02')
