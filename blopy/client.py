09c35bf421b5a770e3c84dbb5b8e2fd910088d65
valves <vitoralvesinfo@gmail.com>
2019-10-05 16:52:57 -0300

    Added new functions

--- blopy/client.py
+++ blopy/client.py
@@ -2,19 +2,18 @@ from blockchain import *
 from server import Server
 import time
 
-bob = Server('localhost', 5000, 15)
+bob = Server('localhost', 5000, 20)
 bob.start()
 
 bob.connect_with_peer('localhost', 6000)
 
-bob.bc.create_genesis_block()
+bob.bc.create_genesis_block('IM BOB')
 data = {'company_name': 'Test', 'company_data': 'New Transaction'}
 bob.bc.new_transaction(data)
 bob.bc.mine()
-last_block = bob.bc.last_block.__dict__
-
-bob.send_to_nodes('FLAG_01')
-# time.sleep(3)
-# bob.send_to_nodes(last_block)
-# time.sleep(3)
-# bob.send_to_nodes('FLAG_01')
+time.sleep(10)
+block = bob.bc.forge_block()
+proof = bob.bc.proof_of_work(block)
+validated_block = bob.bc.validate_block(block, proof)
+time.sleep(3)
+bob.send_to_nodes(validated_block)
