bdcb95157b8cee2dfea3bf7682a53a3f75bc43f8
valves <vitoralvesinfo@gmail.com>
2019-10-09 01:21:47 -0300

    Added new validations

--- blopy/message.py
+++ blopy/message.py
@@ -1,23 +1,27 @@
 import logging
 
 from datetime import datetime
+from pprint import pprint
 
 class Message(object):
     def create(self, msg_type, flag, content=[]):
-        if self.validate(msg_type, flag, content):
-            return {'msg_type': msg_type,
-                    'flag': flag,
-                    'content': content,
-                    'timestamp': str(datetime.now())}
-        else:
+        return {'msg_type': msg_type,
+                'flag': flag,
+                'content': content,
+                'timestamp': str(datetime.now())}
+
+    def validate(self, message):
+        if message['msg_type'] != 'request' and message['msg_type'] != 'response':
+            logging.error('Message: {msg_type} is not valid!')
             return False
 
-    def validate(self, msg_type, flag, content):
-        if isinstance(msg_type, str) and \
-            (msg_type is 'request' or msg_type is 'response') and \
-            isinstance(flag, int) and \
-            (flag > 0 and flag < 100) and  \
-            isinstance(content, list):
-            return True
-        logging.error('Validate: Message raw content variable is not allowed!')
-        return False
+        if message['flag'] < 1 or message['flag'] > 100:
+            logging.error('Message: {flag} is not valid!')
+            return False
+
+        if not (isinstance(message['msg_type'], str) and
+                isinstance(message['flag'], int) and
+                isinstance(message['content'], list)):
+            logging.error('Message: {content} is not valid!')
+            return False
+        return True
