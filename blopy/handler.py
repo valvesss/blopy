7e888170359cd14eeb0be9f40231349368cc30f1
valves <vitoralvesinfo@gmail.com>
2019-10-09 01:21:03 -0300

    Fixed minor refs

--- blopy/handler.py
+++ blopy/handler.py
@@ -1,33 +1,36 @@
 import logging
 
 from message import Message
+from blockchain import Block
 
 class Handler(object):
     def __init__(self, node, data):
         self.node = node
-        self.data_type = data['msg_type']
+        self.data = data
         self.message = Message()
         self.request = Request(self, data)
         self.response = Response(self, data)
+        self.block = Block()
 
     def validate(self):
-        try:
-            if self.message.validate(self.data):
-                self.treat(data)
+        if self.message.validate(self.data):
+            self.treat()
 
     def treat(self):
-        if self.data_type is 'request':
-            self.request.selector()
-        elif self.data_type is 'response':
+        if self.data['msg_type'] == 'request':
             self.response.selector()
+        elif self.data['msg_type'] == 'response':
+            self.request.selector()
         else:
             logging.error('{0}Peer #{1}: received a message not valid!'.format(self.node.type,self.node.index))
             return False
 
 class Response:
-    def __init__(self, node, data):
-        self.node = node
+    def __init__(self, handler, data):
+        self.node = handler.node
         self.data = data
+        self.message = handler.message
+        self.handler = handler
 
     def selector(self):
         if self.data['flag'] == 1:
@@ -40,26 +43,20 @@ class Response:
             else:
                 self.new_block()
         else:
-            logging.error('{0}Peer #{1}: response flag not valid!'.format(self.node.node_index,self.index))
+            logging.error('{0}Peer #{1}: response flag not valid!'.format(self.node.type,self.node.index))
             return False
 
-    def chain_size(self, message):
-        logging.info('{0}Peer #{1}: Response: received a chain size request'.format(self.node.node_index,self.index))
-        if self.node.wait_response == 1:
-            chain_size = self.data['content'][0]
-            self.validate_chain_size(chain_size)
-            self.node.wait_response = 0
-        else:
-            server_chain_size = str(len(self.node._server_.bc.chain))
-            message = self.message.create('response', 1, [server_chain_size])
-            self.node.send(message)
+    def chain_size(self):
+        server_chain_size = len(self.node._server_.bc.chain)
+        message = self.message.create('response', 1, [server_chain_size])
+        self.node.send(message)
+        logging.info('{0}Peer #{1}: Response: sent a chain size request'.format(self.node.type,self.node.index))
 
-    def chain_sync(self, data):
-        logging.info('{0}Peer #{1}: Response: received a chain sync request'.format(self.node.node_index,self.index))
-        blocks = data['content']
-        for block in blocks:
-            if self.node._server_.bc.validate_block(block):
-                self.node._server_.bc.add_block(block)
+    def chain_sync(self):
+        blocks = [self.handler.block.dict_to_json(block) for block in self.node._server_.bc.chain]
+        message = self.handler.message.create('response', 2, blocks)
+        self.node.send(message)
+        logging.info('{0}Peer #{1}: Response: sent a chain sync message'.format(self.node.type,self.node.index))
 
     def new_block(self):
         logging.info('{0}Peer #{1}: Response: received a new block!'.format(self.node.type,self.node.index))
@@ -73,16 +70,6 @@ class Response:
             else:
                 self.return_response(3)
 
-    def append_new_block(self):
-        if self.data['content']:
-            self.node._server_.bc.add_block(self.data['content'])
-
-    def validate_chain_size(self, size):
-        if size > len(self.node._server_.bc.chain):
-            logging.info('{0}Peer #{1}: my chain is smaller!'.format(self.node.node_index,self.index))
-            self.node._server_.bc.chain = []
-            self.request_node_chain()
-
     def return_response(self, flag, cotent=None):
         response = create('response', 3, [cotent])
         self.node.send(response)
@@ -90,8 +77,9 @@ class Response:
 class Request:
     def __init__(self, handler, data):
         self.node = handler.node
-        self.message = handler.message
         self.data = data
+        self.message = handler.message
+        self.handler = handler
 
     def selector(self):
         if self.data['flag'] == 1:
@@ -99,18 +87,24 @@ class Request:
         elif self.data['flag'] == 2:
             self.chain_sync()
         else:
-            logging.error('{0}Peer #{1}: Request: flag not valid!'.format(self.node.type,self.node.index))
+            logging.error('{0}Peer #{1}: response flag not valid!'.format(self.node.type,self.node.index))
             return False
 
     def chain_size(self):
-        message = self.message.create('request', 1, [])
-        if self.node.send(message):
-            self.node.wait_response = 1
-        logging.info('{0}Peer #{1}: Request: sent chain size message'.format(self.node.type,self.node.index))
+        chain_size = self.data['content'][0]
+        self.validate_chain_size(chain_size)
 
     def chain_sync(self):
-        blocks = [dict_to_json(block) for block in self.node._server_.bc.chain]
-        message = create('request', 2, blocks)
-        if self.node.send(message):
-            self.node.wait_response = 1
-        logging.info('{0}Peer #{1}: Request: sent a chain sync message'.format(self.node.type,self.node.index))
+        from pprint import pprint
+        self.node._server_.bc.chain = []
+        for json_block in self.data['content']:
+            block = self.handler.block.json_to_dict(json_block)
+            if self.node._server_.bc.validate_block(block, block['hash']):
+                self.node._server_.bc.add_block(block)
+        pprint(self.node._server_.bc.chain)
+
+    def validate_chain_size(self, size):
+        if size > len(self.node._server_.bc.chain):
+            logging.info('{0}Peer #{1}: my chain is smaller!'.format(self.node.type,self.node.index))
+            self.node._server_.bc.chain = []
+            self.request_node_chain()
