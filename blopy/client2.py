af4d911d461b657a9b20edd070a61e965e55951a
valves <vitoralvesinfo@gmail.com>
2019-10-09 01:20:21 -0300

    Changed send messages schema

--- blopy/client2.py
+++ blopy/client2.py
@@ -1,14 +1,15 @@
 import time
 
-from pprint import pprint
 from blockchain import *
+from pprint import pprint
 from server import Server
+from message import Message
 
-alice = Server('localhost', 6000, 20)
+alice = Server('localhost', 6000, 10)
 alice.start()
+m = Message()
 
 alice.bc.create_genesis_block('IM ALICE')
-alice.send_to_nodes('REQ_01')
 time.sleep(5)
-msg = alice.create_message('request', 1, '')
+msg = m.create('request', 2)
 alice.send_to_nodes(msg)
