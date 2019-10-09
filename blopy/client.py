f88f464139741644bc6b3f0d703e82d44d8c83a5
valves <vitoralvesinfo@gmail.com>
2019-10-09 01:19:47 -0300

    Minor fixes

--- blopy/client.py
+++ blopy/client.py
@@ -1,19 +1,19 @@
-from blockchain import *
-from server import Server
 import time
 
-bob = Server('localhost', 5000, 20)
-bob.start()
+from blockchain import *
+from server import Server
+from message import Message
 
+bob = Server('localhost', 5000, 10)
+bob.start()
+m = Message()
 bob.connect_with_peer('localhost', 6000)
 
 bob.bc.create_genesis_block('IM BOB')
-data = {'company_name': 'Test', 'company_data': 'New Transaction'}
+data = {'company_name': 'It works!', 'company_data': 'Bloco do Bob'}
 bob.bc.new_transaction(data)
 bob.bc.mine()
-time.sleep(10)
+time.sleep(3)
 block = bob.bc.forge_block()
 proof = bob.bc.proof_of_work(block)
 validated_block = bob.bc.validate_block(block, proof)
-time.sleep(3)
-bob.send_to_nodes(validated_block)
