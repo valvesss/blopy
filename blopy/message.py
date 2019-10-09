73ee85dd1afb7afe5f9bf8792983121bdd04d6f0
valves <vitoralvesinfo@gmail.com>
2019-10-09 10:56:10 -0300

    Improved validation for timestamp

--- blopy/message.py
+++ blopy/message.py
@@ -1,16 +1,24 @@
 import logging
 
-from datetime import datetime
 from pprint import pprint
+from datetime import datetime
 
 class Message(object):
     def create(self, msg_type, flag, content=[]):
-        return {'msg_type': msg_type,
-                'flag': flag,
-                'content': content,
-                'timestamp': str(datetime.now())}
+        new_message =  {'msg_type': msg_type,
+                        'flag': flag,
+                        'content': content,
+                        'timestamp': str(datetime.now())}
+        if self.validate(new_message)
 
     def validate(self, message):
+        if not (isinstance(message['msg_type'], str) and
+                isinstance(message['flag'], int) and
+                isinstance(message['content'], list) and
+                isinstance(message['timestamp'], str)):
+            logging.error('Message: {content} is not valid!')
+            return False
+
         if message['msg_type'] != 'request' and message['msg_type'] != 'response':
             logging.error('Message: {msg_type} is not valid!')
             return False
@@ -19,9 +27,11 @@ class Message(object):
             logging.error('Message: {flag} is not valid!')
             return False
 
-        if not (isinstance(message['msg_type'], str) and
-                isinstance(message['flag'], int) and
-                isinstance(message['content'], list)):
-            logging.error('Message: {content} is not valid!')
+        if len(message['timestamp']) < 26 or len(message['timestamp']) > 26
+            return False
+
+        try:
+            datetime.strptime(message['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
+        except:
             return False
         return True
