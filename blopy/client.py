5cd5278cb2085acb524a0eb3a5c5bd0c6b82c2dd
valves <vitoralvesinfo@gmail.com>
2019-10-13 01:22:02 -0300

    Adde new func names

--- blopy/client.py
+++ blopy/client.py
@@ -1,20 +1,24 @@
 import time
 
-from blockchain import *
+# import block
+# from blockchain import Blockchain
+from pprint import pprint
 from server import Server
 from message import Message
 
-bob = Server('localhost', 5000, 20)
+bob = Server('localhost', 5000, 5)
 bob.start()
-m = Message()
-bob.connect_with_peer('localhost', 6000)
 
-bob.bc.create_genesis_block('IM BOB')
-data = {'company_name': 'It works!', 'company_data': 'Bloco do Bob'}
-bob.bc.new_transaction(data)
-bob.bc.mine()
-time.sleep(8)
-bob.bc.forge_block()
-bob.bc.request_add_block()
-time.sleep(12)
-pprint(bob.bc.chain)
+bob.bc.forge_genesis_block('IM BOB')
+# m = Message()
+# bob.connect_with_peer('localhost', 6000)
+
+# bob.bc.create_genesis_block('IM BOB')
+# data = {'company_name': 'It works!', 'company_data': 'Bloco do Bob'}
+# bob.bc.new_transaction(data)
+# bob.bc.mine()
+# time.sleep(8)
+# bob.bc.forge_block()
+# bob.bc.request_add_block()
+# time.sleep(12)
+# pprint(bob.bc.chain)
