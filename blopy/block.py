0306eb82d277d72c94d94d404a3bd6b94e387c9e
valves <vitoralvesinfo@gmail.com>
2019-10-15 11:53:08 -0300

    Removed validation from inside block

--- blopy/block.py
+++ blopy/block.py
@@ -40,34 +40,18 @@ class Block(object):
 
         def __init__(self, block):
             self.block = block
+            self.remove_hash()
 
         def remove_hash(self):
-            if 'hash' in block:
+            if 'hash' in self.block:
                 del self.block['hash']
 
         def keys(self):
-            self.remove_hash()
-            if not all (k in self.block for k in self.block_required_items.keys()):
-                logging.error('Server Blockchain: Block #{} keys are not valid!'.format(self.block['index']))
-                return False
-            return True
+            if self.utils.validate_dict_keys(self.block, self.block_required_items):
+                return True
+            return False
 
         def values(self):
-            self.remove_hash()
-            keys = [k for k in self.block.keys()]
-            for i in range(len(self.block)):
-                if type(self.block[keys[i]]) != self.block_required_items[keys[i]]:
-                    logging.error('Server Blockchain: Block #{} values are not valid!'.format(self.block['index']))
-                    return False
-            return True
-
-        def validate_proof(self):
-            if self.block['index'] == 0:
-                logging.error('Server Blockchain: Block #{} has no valid proof! He\'s genesis!'.format(self.block['index']))
+            if self.utils.validate_dict_values(self.block, self.block_required_items):
                 return True
-            block_hash = self.compute_hash(self.block)
-            if (not (block_hash.startswith('0' * self.pow_difficulty) or
-                    block_hash != self.block['hash'])):
-                logging.error('Server Blockchain: Block #{} has no valid proof!'.format(self.block['index']))
-                return False
-            return True
+            return False
