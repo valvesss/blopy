e679b22a4e1a33dd94c6e7032d905a20c9a5bbff
valves <vitoralvesinfo@gmail.com>
2019-10-15 12:19:22 -0300

    Brought functions for validate block

--- blopy/utils.py
+++ blopy/utils.py
@@ -24,3 +24,27 @@ class Utils(object):
             logging.error('Block: error converting dict to json!')
             return False
         return json_data
+
+    def validate_dict_keys(self, data, base_dict):
+        data_keys = [k for k in data.keys()]
+        base_keys = [k for k in base_dict.keys()]
+        if sorted(data_keys) != sorted(base_keys):
+            logging.error('Server Transaction: Transaction #{} keys are not valid!'.format(data['index']))
+            return False
+        return True
+
+    def validate_dict_values(self, data, base_dict):
+        keys = [k for k in data.keys()]
+        for i in range(len(keys)):
+            if type(data[keys[i]]) != base_dict[keys[i]]:
+                logging.error('Server Transaction: Transaction #{} values are not valid!'.format(data['index']))
+                return False
+        return True
+
+    def validate_proof(self, block):
+        block_hash = self.compute_hash(block)
+        if (not (block_hash.startswith('0' * 2) or
+                block_hash != block['hash'])):
+            logging.error('Server Blockchain: Block #{} has no valid proof!'.format(self.block['index']))
+            return False
+        return True
