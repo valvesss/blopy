73a7c1c9b7d0d41df4f4caba208209a49a60d63b
valves <vitoralvesinfo@gmail.com>
2019-10-18 09:28:24 -0300

    Updated values

--- blopy/announce.py
+++ blopy/announce.py
@@ -6,12 +6,11 @@ class Announce(object):
     def __init__(self, node, data):
         self.node = node
         self.data = data
+        self.selector()
 
     def selector(self):
-        if self.data['flag'] == 20:
+        if self.data['flag'] == 1:
             self.new_block()
 
     def new_block(self):
         block_number = self.data['content'][0]
-        logging.info('Announcement: block #{0} was declared valid!'.format(block_number))
-        self.node.server.write_message('request', 1)
